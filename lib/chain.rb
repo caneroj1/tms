db19352e19d3cb0abf2b92de10307b623d804a6a
caneroj1 <caneroj1@tcnj.edu>
2014-08-23 11:13:00 -0400

    new function to add a more complete graph to the logic graph that will make queries easier, instead of having to traverse the graph.

--- lib/chain.rb
+++ lib/chain.rb
@@ -14,6 +14,7 @@ module TMS
 			link = TMS::Link.new(tokens[0], bases[1])
 			bases[0].create_link(link)
 			@bases.push(bases[0], bases[1])
+			complete_graph(bases, link)
 			"OK!"
 		end
 
@@ -22,6 +23,7 @@ module TMS
 			link = TMS::Link.new(tokens[0], bases[1])
 			bases[0].create_link(link)
 			@bases.push(bases[0], bases[1])
+			complete_graph(bases, link)
 			"Got it!"
 		end
 
@@ -31,12 +33,27 @@ module TMS
 			link = TMS::Link.new(tokens[0], bases[1], some_not ? "not" : nil)
 			bases[0].create_link(link)
 			@bases.push(bases[0], bases[1])
+			complete_graph(bases, link)
 			"I know that now!"
 		end
 
 		def describe(tokens)
 			base = @bases.index { |b| b.noun.eql?(tokens[1]) }
 			@bases[base].describe
+		end
+
+		def complete_graph(bases, new_link) 
+			qualifiers = { "all" => 2, "some" => 1, "no" => 0 }
+
+			if bases[2] ^ bases[3]
+				old_base = bases[2] ? bases[1] : bases[0]
+				new_base = bases[2] ? bases[0] : bases[1]
+
+				old_base.links.select { |link| link != new_link }.each do |link|
+					qualifier = qualifiers[link.qualifier] <= qualifiers[new_link.qualifier] ? link.qualifier : new_link.qualifier
+					new_base.create_link(TMS::Link.new(qualifier, link.base, link.secondary_qualifier))
+				end
+			end
 		end
 
 		private
@@ -48,7 +65,7 @@ module TMS
 			base_1 = ind_1.nil? ? TMS::Base.new(tokens[1]) : @bases.delete_at(ind_1)
 			base_2 = ind_2.nil? ? TMS::Base.new(tokens[pos]) : @bases.delete_at(ind_2)
 			
-			[base_1, base_2]
+			[base_1, base_2, ind_1.nil?, ind_2.nil?]
 		end
 	end
 end
