6c385a4f199d7e318b4c88bdafba4b4f90930386
caneroj1 <caneroj1@tcnj.edu>
2014-08-23 11:22:33 -0400

    cleaned up the chain class.

--- lib/chain.rb
+++ lib/chain.rb
@@ -11,47 +11,38 @@ module TMS
 
 		def all(tokens)
 			bases = prepare_data(tokens, false)
-
-			link_1 = TMS::Link.new(tokens[0], bases[1])
-			link_2 = TMS::Link.new(tokens[0], bases[0])
-			bases[0].create_link(link_1)
-			bases[1].create_link(link_2)
-
-			@bases.push(bases[0], bases[1])
-			complete_graph(bases, link_1)
+			add_to_graph(tokens, bases)
 			"OK!"
 		end
 
 		def no(tokens)
 			bases = prepare_data(tokens, false)
-
-			link_1 = TMS::Link.new(tokens[0], bases[1])
-			link_2 = TMS::Link.new(tokens[0], bases[0])
-			bases[0].create_link(link_1)
-			bases[1].create_link(link_2)
-
-			@bases.push(bases[0], bases[1])
-			complete_graph(bases, link_1)
+			add_to_graph(tokens, bases)
 			"Got it!"
 		end
 
 		def some(tokens)
 			some_not = tokens.count.eql?(5)
 			bases = prepare_data(tokens, some_not)
-
-			link_1 = TMS::Link.new(tokens[0], bases[1], some_not ? "not" : nil)
-			link_2 = TMS::Link.new(tokens[0], bases[0], some_not ? "not" : nil)
-			bases[0].create_link(link_1)
-			bases[1].create_link(link_2)
-
-			@bases.push(bases[0], bases[1])
-			complete_graph(bases, link_1)
+			add_to_graph(tokens, bases)
 			"I know that now!"
 		end
 
 		def describe(tokens)
 			base = @bases.index { |b| b.noun.eql?(tokens[1]) }
 			@bases[base].describe
+		end
+
+		private
+		def prepare_data(tokens, is_some_not_asssertion)
+			ind_1 = @bases.index { |b| b.noun.eql?(tokens[1]) }
+			pos = is_some_not_asssertion ? 4 : 3
+			ind_2 = @bases.index { |b| b.noun.eql?(tokens[pos]) }
+
+			base_1 = ind_1.nil? ? TMS::Base.new(tokens[1]) : @bases.delete_at(ind_1)
+			base_2 = ind_2.nil? ? TMS::Base.new(tokens[pos]) : @bases.delete_at(ind_2)
+			
+			[base_1, base_2, ind_1.nil?, ind_2.nil?]
 		end
 
 		def complete_graph(bases, new_link) 
@@ -69,16 +60,14 @@ module TMS
 			end
 		end
 
-		private
-		def prepare_data(tokens, is_some_not_asssertion)
-			ind_1 = @bases.index { |b| b.noun.eql?(tokens[1]) }
-			pos = is_some_not_asssertion ? 4 : 3
-			ind_2 = @bases.index { |b| b.noun.eql?(tokens[pos]) }
+		def add_to_graph(tokens, bases)
+			link_1 = TMS::Link.new(tokens[0], bases[1], tokens.count.eql?(5) ? "not" : nil)
+			link_2 = TMS::Link.new(tokens[0], bases[0], tokens.count.eql?(5) ? "not" : nil)
+			bases[0].create_link(link_1)
+			bases[1].create_link(link_2)
 
-			base_1 = ind_1.nil? ? TMS::Base.new(tokens[1]) : @bases.delete_at(ind_1)
-			base_2 = ind_2.nil? ? TMS::Base.new(tokens[pos]) : @bases.delete_at(ind_2)
-			
-			[base_1, base_2, ind_1.nil?, ind_2.nil?]
+			@bases.push(bases[0], bases[1])
+			complete_graph(bases, link_1)
 		end
 	end
 end
